491197312535f1ec87d4517cfee54bdf0cc5fd41
BartKK <Bartkukulka@gmail.com>
2017-04-01 16:27:29 +0200

    changing scope

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('myAppController', ['$scope', function myAppController($scope){
   $scope.name = 'Kuba';
+
+$scope.$watch('surname', function (newVal, oldVal) {
+  console.log('New value is ' + newVal);
+});
+
+setTimeout(function () {
   $scope.surname = 'Matyka';
-  $scope.greetings = 'Hello'
+  $scope.greetings = 'Hello';
+  $scope.$apply()
+}, 1000)
+
+
 }]);
