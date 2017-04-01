f3980f63595eea4a35599c20b5525443b5fef503
BartKK <Bartkukulka@gmail.com>
2017-04-01 16:05:35 +0200

    initialize app and register controller

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var myApp = angular.module('myApp', []);
+
+
+myApp.controller('myAppController', ['$scope', function myAppController($scope){
+  $scope.name = 'Kuba';
+  $scope.surname = 'Matyka';
+  $scope.greetings = 'Hello'
+}]);
