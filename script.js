f525f49505fbe45c62d6c3bed13ae24c7e4a9ee9
BartKK <Bartkukulka@gmail.com>
2017-04-02 11:42:23 +0200

    ng-click with function

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ var myApp = angular.module('myApp', []);
 
 
 myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $http) {
+$scope.sortField = 'population';
+$scope.reverse = true;
+
+
 $http.get('countries.json').success(function (data) {
   $scope.countries = data;
-})
+});
 
+$scope.sortField = 'population';
+$scope.reverse = true;
+$scope.sortFields = function (data) {
+  $scope.sortField = data;
+  $scope.reverse = !$scope.reverse;
+}
 }]);
