e1ec98372a1f377db7cb5d3f61bb9101822664df
BartKK <Bartkukulka@gmail.com>
2017-04-02 10:41:27 +0200

    mock data in json file and fetch with http service

--- script.js
+++ script.js
@@ -2,13 +2,9 @@
 var myApp = angular.module('myApp', []);
 
 
-myApp.controller('myAppController', ['$scope', function myAppController($scope){
-$scope.countries = [
-    {name: 'China', population: 13000000},
-    {name: 'India', population: 198234672},
-    {name: 'United States', population: 248091893},
+myApp.controller('myAppCtrl', ['$scope', '$http', function myAppCtrl($scope, $http) {
+$http.get('countries.json').success(function (data) {
+  $scope.countries = data;
+})
 
-
-
-]
 }]);
